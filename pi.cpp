82c9df500164cbc2af0cfac98082275bc6cd9d36
benfogiel <52505296+benfogiel@users.noreply.github.com>
2019-07-05 22:20:28 -0700

    Update pi.cpp

--- pi.cpp
+++ pi.cpp
@@ -17,20 +17,20 @@ int main(){
 
   while(n != -1){
     pi = 0.0;
-    cout << "Enter the value of the parameter 'n' in the Leibniz formula (or -1 to quit):" << endl;
+    cout << "Enter the value of the parameter 'n' in the Leibniz formula (or -1 to quit): " << endl;
     cin >> n;
     if (n == -1) return 0;
     term = n + 1;
     for (int i = 0; i < term; i++){
-      pi = pi + pow(-1.0, i)/(2.0*i+1.0);
+      pi += (pow(-1.0, i)/(2.0*i+1.0));
     }
-    pi = pi * 4.0;
+    pi *= 4.0;
     if (n == 0){
       t = "term";
     }else{
       t = "terms";
     }
-    cout << "The approximate value of pi using " << term << " " << t << " is: " << setprecision(4) << static_cast<double>(pi) << endl;
+    cout << "The approximate value of pi using " << term << " " << t << " is: " << setprecision(3) << fixed << pi << endl;
   }
   return 0;
 }
